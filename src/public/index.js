aeb0812f4e4e7139e33d6b1583e601ba934bdb63
BeingNeophyte <42290043+BeingNeophyte@users.noreply.github.com>
2018-09-28 11:31:00 -0700

    Update index.js

--- src/public/index.js
+++ src/public/index.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
         $("#message").val("");
 
         //send message
-        $.post( "https://neophyte.cf:5000/message", { message } );
+        $.post( "http://neophyte.cf:5000/message", { message } );
     });
 
     function onMessageAdded(data) {
