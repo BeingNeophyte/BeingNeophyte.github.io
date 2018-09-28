d4ab889b695e8cf868a9294274b1bd60a43f6302
BeingNeophyte <42290043+BeingNeophyte@users.noreply.github.com>
2018-09-28 10:47:46 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,26 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
 var Pusher = require('pusher');
 
-var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-var pusher = new Pusher({ appId: '610429', key: '79530d022fb171a3424f', secret: 'ccd964ce00d1d6c1430e', cluster: 'ap2',encrypted: true });
-
-app.post('/message', function(req, res) {
-  var message = req.body.message;
-  pusher.trigger( 'public-chat', 'message-added', { message: message });
-  res.sendStatus(200);
+var pusher = new Pusher({
+  appId: '610433',
+  key: '48dea147161a3f599d6d',
+  secret: '62f6664d19664773e8fc',
+  cluster: 'ap2',
+  encrypted: true
 });
 
-app.get('/',function(req,res){      
-     res.sendFile('/public/index.html', {root: 'src' });
-});
-
-app.use(express.static('src' + '/public'));
-
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-  console.log(`app listening on port ${port}!`)
+pusher.trigger('my-channel', 'my-event', {
+  "message": "hello world"
 });
